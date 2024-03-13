5f736b25441b4479c818e2a73e09db42e0767d9e
Rice <patricemaxwell@yahoo.com>
2024-03-13 00:49:24 -0400

    Delete server.py

--- server.py
+++
@@ -1,8 +0,0 @@
-from flask_app import app
-
-from flask_app.controllers import users
-from flask_app.controllers import messages
-from flask_app.controllers import songs
-
-if __name__ == '__main__':
-    app.run(debug=True)
